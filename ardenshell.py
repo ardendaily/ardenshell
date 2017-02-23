e1d7fd8477ade39fcf88601ed56a4d45b5b52bd0
Arden Daily <ardendaily@gmail.com>
2017-02-23 13:15:21 -0800

    added per-loop function binding with addLoopMethod. you can optionally attach arguments to your function call. does not return values-- that is up next

--- ardenshell.py
+++ ardenshell.py
@@ -11,6 +11,8 @@ class ArdenShell:
 	'''
 	def __init__(self):
 		self.bindCommands = {}
+		self.loopMethods = {}
+		self.namespace = None
 		self.exitText = "Bye!"
 		self.pcresponse = ""
 		self.err = "Command not found."
@@ -18,13 +20,16 @@ class ArdenShell:
 		self.eBanner = "" #displays after every interaction.
 		self.reticule = "> " #beckon input
 
+	def setnamespace(self, namespace):
+		self.namespace = namespace
+
 	def userin(self, _input):
 		''' Handle user input '''
 		_input = _input.lower()
 		_input = _input.split(" ")
 		if _input[0] in self.bindCommands.keys():
 			if len(_input) > 1:
-				return self.bindCommands[_input[0]]( _input[1:]) 
+				return self.bindCommands[_input[0]]( _input[1:])
 			else:
 				return self.bindCommands[_input[0]]()
 		else:
@@ -50,6 +55,28 @@ class ArdenShell:
 			methObject = getattr(namespace, methString)
 			self.bindCommands[key] = methObject
 
+	def addLoopMethod( self, methString, methArgs=None, returnTo=None, namespace=sys.modules[__name__]):
+		'''
+		Append method to loop, exec in order. 
+		'''
+
+		self.loopMethods[methString] = {}
+
+		methObject = getattr(namespace, methString)
+		self.loopMethods[methString]['function'] = methObject
+		
+		args = []
+
+		if methArgs is not None:
+			if not isinstance(methArgs, list):
+				methArgs = [methArgs]
+
+			for argument in methArgs:
+				#arguments are everything else
+				args.append(argument)
+
+		self.loopMethods[methString]['args'] = args
+
 	def setBanner( self, banner):
 		self.banner = banner
 
@@ -59,7 +86,7 @@ class ArdenShell:
 	def setExitText( self, _exittext):
 		self.exitText = _exittext
 
-	def loop(self):
+	def loop(self, noIO=False):
 		print ""
 		print ""
 		print self.banner
@@ -70,8 +97,18 @@ class ArdenShell:
 			try:
 				print self.pcresponse
 				print self.eBanner
-				user = raw_input(self.reticule)
-				self.pcresponse = self.userin( user )
+
+				for methArgs in self.loopMethods.keys():
+
+					if len(self.loopMethods[methArgs]['args']) > 0 :
+						self.loopMethods[methArgs]['function']( 
+							self.loopMethods[methArgs]['args'] 
+						)
+					else:
+						self.loopMethods[methArgs]['function']()
+				if not noIO:
+					user = raw_input(self.reticule)
+					self.pcresponse = self.userin( user )
 			except KeyboardInterrupt:
 				# Catch 
 				self.die()
